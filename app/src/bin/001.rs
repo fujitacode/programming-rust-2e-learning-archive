fdbf56725a14cfae2044455536d22390828fcb80
fujitacode <riot.jack.bbb@gmail.com>
2026-03-08 20:50:07 +0900

    appに移動

---
+++ app/src/bin/001.rs
@@ -0,0 +1,8 @@
+// println!マクロのこと
+// 関数を入れたい場合("{}", 関数)にする。
+// ただ文字をいれたいなら("文字列")でも出力できる。
+fn main() {
+    let word = "Hi,I am itou";
+    println!("{}", word);
+    println!("Hi,I am itou");
+}
